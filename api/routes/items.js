38b322c57ed97f227d7085d48d85fd5db937f5e4
Ivan Martinez <ivmartinezc@gmail.com>
2023-01-21 12:56:51 +0100

    move everything to api folder

--- api/routes/items.js
+++ api/routes/items.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const Item = require("../models/Item");
+const Item = require("../api/lib/models/Item");
 
 router.get("/", async (req, res, next) => {
   try {
