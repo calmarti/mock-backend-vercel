64d28afeaea0db6ceae0a34c1d4af09b71816320
Ivan Martinez <ivmartinezc@gmail.com>
2023-01-21 21:34:37 +0100

    adjust app.get() in index.js

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.get("/", (req,res)=> {
+app.get("/api", (req,res)=> {
     res.send('Trofiplast backend succesfully deployed...');  
 })
 
