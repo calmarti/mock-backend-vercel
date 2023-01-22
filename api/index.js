c77cc38a76a31e4c6a1bdd7ca7110139fa24d493
Ivan Martinez <ivmartinezc@gmail.com>
2023-01-22 09:33:08 +0100

    mock commit

--- api/index.js
+++ api/index.js
@@ -32,7 +32,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api", (req,res)=> {
     res.send('Trofiplast backend succesfully deployed...');  
-})
+
+  })
 
 //rutas del api
 
