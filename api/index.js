ae2d831df0c31a07d235116d5034728e26ff1b2f
Ivan Martinez <ivmartinezc@gmail.com>
2023-01-22 10:26:03 +0100

    change path in middleware

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.get("/api", (req,res)=> {
+app.get("/", (req,res)=> {
     res.send('Trofiplast backend succesfully deployed...');  
 
   })
